d42bdb462b0bb5295a74995f8d089d85f5fd7142
Sandy Oliveira <38080685+SandyOliveira@users.noreply.github.com>
2022-08-22 15:53:58 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
+//crie as variaveis dos seus personagens aqui
 var box;
+
+
+function preload(){
+  //carregue as imagens dos personagens aqui
+}
 
 function setup() {
   createCanvas(800,400);
+  //crie os sprites aqui
   box = createSprite(200,200,50,80);
   box.shapeColor = "green";
 
